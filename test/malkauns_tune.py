75e61cc5c8f1c29a1beacddeb3139083e3d821c1
Shamit Verma <github@shamit.in>
2023-06-20 16:41:47 +0530

    Pick Vaadi and Samvaadi with higher probability

--- test/malkauns_tune.py
+++ test/malkauns_tune.py
@@ -26,22 +26,31 @@ taar_saptak_octave = 4
 octaves = [mandra_saptak_octave, madhya_saptak_octave, taar_saptak_octave]
 
 notes = []
+note_weights = []
 
 for octave in octaves:
     for note in malkauns_raag_notes:
         notes.append({"octave": octave, "note": note})
+        if note == vaadi_swar:
+            note_weights.append(50)
+        else:
+            if note == samvaadi_swar:
+                note_weights.append(30)
+            else:
+                note_weights.append(10)
 
-durations = [1, 0.5, 0.25, 0.125]
+durations = [1, 0.5, 0.5, 0.25, 0.25, 0.125]
 scr = Score()
 p = scr.add_child(Part('P1', name='Part 1'))
 measure = p.add_measure();
 
-bars = 36
+bars = 72
 num_notes = bars * 4
 
 generated_notes = []
 
 cur_idx = None
+idx_options = list(range(0, len(notes)))
 for i in range(num_notes):
     next_note = None
     if cur_idx is not None:
@@ -57,9 +66,9 @@ for i in range(num_notes):
         if choice is not None:
             cur_idx = cur_idx + choice
         else:
-            cur_idx = random.randrange(len(notes))
+            cur_idx = random.choices(idx_options, weights=note_weights, k=1)[0]
     else:
-        cur_idx = random.randrange(len(notes))
+        cur_idx = random.choices(idx_options, weights=note_weights, k=1)[0]
     next_note = notes[cur_idx]
     generated_notes.append(next_note)
 
@@ -75,15 +84,25 @@ def get_note(hindi_note, octave, duration):
 
 current_duration = random.choice(durations)
 change_duration_or_not = [None, None, 1, None, None, None]
-for n in generated_notes:
-    hindi_note = n['note']
-    hindi_octave = n['octave']
-    en_note = en_note = hindi_to_western_mapping[hindi_note]
-    piano_octave = hindi_octave + octave_offset[en_note]
-    if random.choice(change_duration_or_not):
-        current_duration = random.choice(durations)
-    note = get_note(hindi_note, piano_octave, current_duration)
-    note.add_lyric(hindi_note)
-    p.add_chord(note)
+non_1_duration_count = 0
 
-scr.export_xml('../data/generated_songs/malkauns/malkauns_tune_1.xml')
+notes_in_a_beat = [1, 2]
+
+# Each iteration of this loop represents a beat
+while len(generated_notes) > 0:
+    notes = []
+    num_notes = random.choice(notes_in_a_beat)
+    for i in range(num_notes):
+        if generated_notes:
+            notes.append(generated_notes.pop(0))
+    duration = 1.0 / len(notes)
+    for n in notes:
+        hindi_note = n['note']
+        hindi_octave = n['octave']
+        en_note = en_note = hindi_to_western_mapping[hindi_note]
+        piano_octave = hindi_octave + octave_offset[en_note]
+        note = get_note(hindi_note, piano_octave, duration)
+        note.add_lyric(hindi_note)
+        p.add_chord(note)
+
+scr.export_xml('../data/generated_songs/malkauns/malkauns_tune_1_1.xml')
